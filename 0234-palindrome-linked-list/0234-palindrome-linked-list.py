e6c899c4437967d387447a2bdf4fc9d098b9efb2
kaknateganesh24 <kaknateganesh24@gmail.com>
2026-07-30 23:34:43 +0530

    Time: 154 ms (10.9%), Space: 65.7 MB (97.17%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isPalindrome(self, head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        current = slow
+
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+
+        first = head
+        second = prev
+
+        while second:
+            if first.val != second.val:
+                return False
+            first = first.next
+            second = second.next
+
+        return True
